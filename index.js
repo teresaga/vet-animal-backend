b44a8080b3b0fd702955b80ca520376a943feea5
Teresa <teresagalaviza@gmail.com>
2018-11-26 23:13:46 -0700

    última versión

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ var app = require('./app');
 var port = 3789;
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/vet_animal')
+mongoose.connect('mongodb://teresagalaviz:teresa1997@ds051665.mlab.com:51665/vet_animal')
 	.then(() => {
 		console.log("La conexión a la base de datos vet_animal se ha realizado correctamente..");
 		
 		//Creacion del servidor
-		app.listen(port, () => {
+		app.listen(process.env.PORT || port, () => {
 			console.log("El servidor local con Node y Express esta corriendo correctamente...")
 		});
 	})
